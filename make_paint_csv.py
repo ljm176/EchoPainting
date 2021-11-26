160fbdf5144224e4de56a090db43b73110091679
Lachlan Munro <43517811+ljm176@users.noreply.github.com>
2021-11-26 15:10:18 +0100

    Add code and template

---
+++ make_paint_csv.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 27 17:37:29 2021
+
+@author: lajamu
+"""
+
+import openpyxl, csv
+
+letters = []
+for i in range(65, 91):
+    letters.append(chr(i))
+
+for i in range(6):
+    letters.append(letters[0] + letters[i])
+
+plate1536 = []
+
+for i in range(1, 49):
+    for letter in letters:
+        plate1536.append(letter + str(i))
+
+
+wb = openpyxl.load_workbook(filename = "template.xlsx")
+
+ws = wb.active
+
+vals = []
+
+for col in ws.iter_cols():
+    for cell in col:
+        vals.append(cell.value)
+
+source_dict = {1 : "A1", 2: "B1", 3:"C1", 4 : "D1", 5 : "E1"}
+
+transfers = [[ "Source Well"," Destination Well", "Transfer Volume"]]
+
+for s, d in zip(vals, plate1536):
+    if s != None:
+        transfers.append([source_dict[s], d, 5])
+
+
+csvFile = "Painting.csv"
+with open(csvFile, "w", newline="") as outputcsv:
+    echoWriter = csv.writer(outputcsv, delimiter=",")
+    for t in transfers:
+        echoWriter.writerow(t)
